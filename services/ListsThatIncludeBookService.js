46353f9b92a63cd07655786c9345763436d990f8
Lucy Johnston <ljohnston931@gmail.com>
2020-07-26 17:57:49 -0600

    ListThatIncludeBookService with tests

--- services/ListsThatIncludeBookService.js
+++ services/ListsThatIncludeBookService.js
@@ -1,128 +1,87 @@
-const http = require("../http");
-const _ = require("lodash");
-const cheerio = require("cheerio");
-const db = require("../models/index");
-const AuthorService = require("./AuthorService");
-const AuthorServiceInstance = new AuthorService();
+const http = require('../http')
+const cheerio = require('cheerio')
+const db = require('../models/index')
 
 class HtmlParser {
-  constructor(document) {
-    this.$ = cheerio.load(document);
-  }
-
-  getListsOnPage() {
-    let lists = [];
-    this.$(".cell").each((listIndex, cell) => {
-      const listTitle = this.$(cell).find(".listTitle");
-      lists[listIndex] = {
-        list_title: listTitle.text(),
-        list_href: listTitle.attr("href").split("#")[0],
-      };
-    });
-
-    return lists;
-  }
-
-  getTotalPages() {
-    const nextPage = this.$("a.next_page");
-
-    if (!nextPage.length) {
-      if (this.$(".cell").length) {
-        return 1;
-      }
-      return 0;
+    constructor(document) {
+        this.$ = cheerio.load(document)
     }
 
-    const lastPage = parseInt(nextPage.prev().text());
-    return lastPage;
-  }
+    getListsOnPage() {
+        let lists = []
+        this.$('.cell').each((listIndex, cell) => {
+            const listTitle = this.$(cell).find('.listTitle')
+            lists[listIndex] = {
+                list_title: listTitle.text(),
+                list_href: listTitle.attr('href').split('#')[0],
+            }
+        })
+
+        return lists
+    }
+
+    getTotalPages() {
+        const nextPage = this.$('a.next_page')
+
+        if (!nextPage.length) {
+            if (this.$('.cell').length) {
+                return 1
+            }
+            return 0
+        }
+
+        const lastPage = parseInt(nextPage.prev().text())
+        return lastPage
+    }
 }
 
 class ListSThatIncludeBookService {
-  constructor(bookId) {
-    this.bookId = bookId;
-  }
-
-  async scrapeBookListsPage(page) {
-    const resp = await http.medium.get(
-      `https://www.goodreads.com/list/book/${this.bookId}?page=${page}`
-    );
-    console.log("gotten");
-    const parser = new HtmlParser(resp.data);
-    let lists = parser.getListsOnPage();
-    let totalPages = null;
-    if (page === 1) {
-      totalPages = parser.getTotalPages();
+    constructor(bookId) {
+        this.bookId = bookId
     }
-    return {
-      totalPages: totalPages,
-      lists: lists,
-    };
-  }
 
-  async getListsForBookFromGoodreads(bookId) {
-    const firstPageResp = await http.medium.get(
-      `https://www.goodreads.com/list/book/${bookId}`
-    );
-
-    const parser = new HtmlParser(firstPageResp.data);
-    const firstPageLists = parser.getListsOnPage();
-    if (firstPageLists.length === 0) {
-      console.log("no lists for", bookId);
-      await db.lists.create({
-        book_id: bookId,
-        list_title: null,
-        list_href: null,
-      });
-      return [];
+    async areBookListsInDatabase() {
+        const bookLists = await db.lists.findOne({
+            where: { book_id: this.bookId },
+        })
+        return bookLists !== null
     }
-    const totalPages = parser.getTotalPages();
-    const otherPages = _.range(2, totalPages + 1, 1);
-    const otherPagesPromises = otherPages.map(async (pageNumber) =>
-      http.medium.get(
-        `https://www.goodreads.com/list/book/${bookId}?page=${pageNumber}`
-      )
-    );
-    const otherPagesResp = await Promise.all(otherPagesPromises);
-    const otherPagesLists = otherPagesResp.map((resp) => {
-      const parser = new HtmlParser(resp.data);
-      return parser.getListsOnPage();
-    });
-    console.log("got lists for", bookId);
-    const bookLists = firstPageLists.concat(_.flatten(otherPagesLists));
 
-    await db.lists.bulkCreate(
-      bookLists.map((list) => {
+    async scrapeBookListsPage(page) {
+        const resp = await http.medium.get(
+            `https://www.goodreads.com/list/book/${this.bookId}?page=${page}`
+        )
+        const parser = new HtmlParser(resp.data)
+        let lists = parser.getListsOnPage()
+        let totalPages = null
+        if (page === 1) {
+            totalPages = parser.getTotalPages()
+        }
         return {
-          book_id: bookId,
-          list_title: list.list_title,
-          list_href: list.list_href,
-        };
-      })
-    );
+            totalPages: totalPages,
+            lists: lists,
+        }
+    }
 
-    return bookLists;
-  }
+    async cacheBookLists(bookLists) {
+        if (bookLists.length) {
+            await db.lists.bulkCreate(
+                bookLists.map(list => {
+                    return {
+                        book_id: this.bookId,
+                        list_title: list.list_title,
+                        list_href: list.list_href,
+                    }
+                })
+            )
+        } else {
+            await db.lists.create({
+                book_id: this.bookId,
+                list_title: null,
+                list_href: null,
+            })
+        }
+    }
 }
 
-module.exports = ListSThatIncludeBookService;
-
-// const { lists } = require("../models")
-
-// constructor (bookId)
-
-// areBookListsInDatabase {
-//     return boolean
-// }
-
-// scrapeBookListsPage(page) {
-//     data = scrapedData
-//     if page = 1 {
-//         data.totalPages = totalPages
-//     }
-//     return data
-// }
-
-// cacheBookLists(lists) {
-//     store in database
-// }
+module.exports = ListSThatIncludeBookService
--- test/listsThatIncludeBook.test.js
+++ test/listsThatIncludeBook.test.js
@@ -1,42 +1,92 @@
-const chai = require("chai");
-const expect = chai.expect;
-const CONSTANTS = require("./TESTING_CONSTANTS.json");
-//process.env.NODE_ENV = "test";
-const ListsThatIncludeBookService = require("../services/ListsThatIncludeBookService");
+const chai = require('chai')
+const expect = chai.expect
+const CONSTANTS = require('./TESTING_CONSTANTS.json')
+const ListsThatIncludeBookService = require('../services/ListsThatIncludeBookService')
+const db = require('../models/index')
 
-describe("ListsThatIncludeBookService", () => {
-  // describe("#areBooksListsInDatabase", () => {
-  //   it("should return true if book lists are loaded in database", () => {
-  //     expect([1, 2, 3].indexOf(4)).to.equal(-1);
-  //   });
-  //   it("should return false if book lists are not loaded in database", () => {
-  //       expect([1, 2, 3].indexOf(4)).to.equal(-1);
-  //     });
-  // });
+describe('ListsThatIncludeBookService', () => {
+    describe('#areBooksListsInDatabase', () => {
+        afterEach(async () => {
+            await db.lists.destroy({
+                where: { book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS },
+            })
+        })
+        it('should return true if book lists are loaded in database', async () => {
+            await db.lists.create({
+                book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS,
+                list_title: null,
+                list_href: null,
+            })
+            const serviceInstance = new ListsThatIncludeBookService(CONSTANTS.BOOK_ID_WITH_NO_LISTS)
+            const res = await serviceInstance.areBookListsInDatabase()
+            expect(res).to.be.true
+        })
+        it('should return false if book lists are not loaded in database', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(CONSTANTS.BOOK_ID_WITH_NO_LISTS)
+            const res = await serviceInstance.areBookListsInDatabase()
+            expect(res).to.be.false
+        })
+    })
 
-  describe("#scrapeBookListsPage", () => {
-    it("should return empty array if no lists on page", async () => {
-      const serviceInstance = new ListsThatIncludeBookService(
-        CONSTANTS.BOOK_ID_WITH_NO_LISTS
-      );
-      const res = await serviceInstance.scrapeBookListsPage(1);
-      expect(res.totalPages).to.equal(0);
-      expect(res.lists).to.have.length(0);
-    });
-    it("should array with data and total pages if first page", async () => {
-      const serviceInstance = new ListsThatIncludeBookService(
-        CONSTANTS.BOOK_ID_WITH_MANY_LISTS
-      );
-      const res = await serviceInstance.scrapeBookListsPage(1);
-      expect(res.totalPages).to.be.greaterThan(10);
-      expect(res.lists).to.have.length(30);
-    }).timeout(3000);
-    it("should array with data and no total pages if not first page", async () => {
-      const serviceInstance = new ListsThatIncludeBookService(
-        CONSTANTS.BOOK_ID_WITH_MANY_LISTS
-      );
-      const res = await serviceInstance.scrapeBookListsPage(5);
-      expect(res.lists).to.have.length(30);
-    }).timeout(3000);
-  });
-});
+    describe('#scrapeBookListsPage', () => {
+        it('should return empty array if no lists on page', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(CONSTANTS.BOOK_ID_WITH_NO_LISTS)
+            const res = await serviceInstance.scrapeBookListsPage(1)
+            expect(res.totalPages).to.equal(0)
+            expect(res.lists).to.have.length(0)
+        })
+        it('should array with data and total pages if first page', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(
+                CONSTANTS.BOOK_ID_WITH_MANY_LISTS
+            )
+            const res = await serviceInstance.scrapeBookListsPage(1)
+            expect(res.totalPages).to.be.greaterThan(10)
+            expect(res.lists).to.have.length(30)
+        }).timeout(3000)
+        it('should array with data and no total pages if not first page', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(
+                CONSTANTS.BOOK_ID_WITH_MANY_LISTS
+            )
+            const res = await serviceInstance.scrapeBookListsPage(5)
+            expect(res.lists).to.have.length(30)
+        }).timeout(3000)
+    })
+
+    describe('#cacheBookLists', () => {
+        beforeEach(async () => {
+            await db.lists.destroy({
+                where: { book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS },
+            })
+        })
+        afterEach(async () => {
+            await db.lists.destroy({
+                where: { book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS },
+            })
+        })
+
+        it('should add book lists to database', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(CONSTANTS.BOOK_ID_WITH_NO_LISTS)
+            await serviceInstance.cacheBookLists(CONSTANTS.BOOK_LISTS)
+
+            const bookListsInDatabase = await db.lists.findAll({
+                where: { book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS },
+            })
+
+            expect(bookListsInDatabase.length).to.equal(1)
+            expect(bookListsInDatabase[0].list_title).to.not.be.null
+            expect(bookListsInDatabase[0].list_href).to.not.be.null
+        })
+
+        it('should add a null entry to database if empty list', async () => {
+            const serviceInstance = new ListsThatIncludeBookService(CONSTANTS.BOOK_ID_WITH_NO_LISTS)
+            await serviceInstance.cacheBookLists([])
+
+            const bookListsInDatabase = await db.lists.findAll({
+                where: { book_id: CONSTANTS.BOOK_ID_WITH_NO_LISTS },
+            })
+
+            expect(bookListsInDatabase.length).to.equal(1)
+            expect(bookListsInDatabase[0].list_href).to.be.null
+        })
+    })
+})
