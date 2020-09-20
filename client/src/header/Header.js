a1991c43c3ed39e6def49b6e5ae87c03b78ef926
Lucy Johnston <ljohnston931@gmail.com>
2020-09-19 23:39:29 -0600

    Change header

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -4,7 +4,8 @@ import "./header.css";
 const Header = () => {
   return (
     <section id="header">
-      <div className="title">Goodreads Analyzer</div>
+      <div className="title">Find a new favorite book!</div>
+      <div className="subheader">Type 2-3 of your favorite books into the search bar.</div>
     </section>
   );
 };
